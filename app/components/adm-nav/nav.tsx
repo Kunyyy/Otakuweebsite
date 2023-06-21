7e267173655d7dbd93f5259d52fd0c87a1581dff
unknown <alfinch07@gmail.com>
2023-06-21 13:10:23 +0700

    last

--- app/components/adm-nav/nav.tsx
+++ app/components/adm-nav/nav.tsx
@@ -6,7 +6,6 @@ function Nav() {
     return (
         <div className="flex flex-col gap-4 text-center">
             <Link href="../Adm/Lightnovel" className={style['link']}>Lightnovel</Link>
-            <Link href="../Adm/Anime" className={style['link']}>Anime</Link>
             <Link href="../Adm/AnimeStatus" className={style['link']}>AnimeStatus</Link>
             <Link href="../Adm/Manga" className={style['link']}>Manga</Link>
         </div>
